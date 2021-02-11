65fcf96da02fc10f3ba4b3595d1e585d7deee682
joshwilson00 <joshuawilson1605@gmail.com>
2021-02-11 11:57:47 +0000

    working

---
+++ server/middleware/checkJWT.js
@@ -0,0 +1,17 @@
+import jwt from 'express-jwt';
+import jwksRsa from 'jwks-rsa';
+
+export function checkJWT(req, res, next) {
+  console.log(process.env.AUTH0_DOMAIN)
+  jwt({
+    secret: jwksRsa.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: `${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+    }),
+    audience: `${process.env.AUTH0_AUDIENCE}`,
+    issuer: `${process.env.AUTH0_DOMAIN}`,
+    algorithms: ['RS256']
+  });
+}
--- server/server.js
+++ server/server.js
@@ -1,1 +1,34 @@
 import express from 'express';
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
+import cors from 'cors';
+
+// import { checkJWT } from './middleware/checkJWT';
+// import { auth } from ('express-openid-connect');
+require('dotenv').config()
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://dev-fpx4zdsj.eu.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'https://banking',
+  issuer: 'https://dev-fpx4zdsj.eu.auth0.com/',
+  algorithms: ['RS256']
+});
+
+
+app.get('/', jwtCheck, (req, res) =>{
+    console.log(process.env.AUTH0_DOMAIN);
+    res.send({message: "hello"})})
+
+  
+
+
+app.listen(5000, ()=> {console.log('Listenting on PORT 5000')})
