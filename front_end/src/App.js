5d832b15cfa90a5664a87860eccc9e3c58123d40
Tom Roman <tom@tomroman.co.uk>
2021-02-11 11:20:45 +0000

    Basic React Login

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import UserProfile from './UserProfile';
+import LogoutButton from './LogoutButton';
+import LoginButton from './LoginButton';
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { isAuthenticated } = useAuth0();
+
+  if (isAuthenticated) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <UserProfile />
+          <LogoutButton />
+        </header>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <LoginButton />
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- front_end/src/index.js
+++ front_end/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 import reportWebVitals from './reportWebVitals';
+require('dotenv').config()
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT}
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById('root')
 );
 
