94ca4c20f3b4737cb2d392706a843531a1e304db
joshwilson00 <joshuawilson1605@gmail.com>
2021-02-12 09:59:34 +0000

    Email setup

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import User from "./models/User";
 import jwtCheck from "./middleware/checkJWT";
 import signupController from "./controllers/signupController";
-
+import Mailer from './utils/Mailer';
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -24,6 +24,13 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signup", signupController);
+
+app.post('/invite', (req, res) => {
+  const mailer = new Mailer(req.body.from);
+  mailer.sendEmailInvite(req.body.to);
+  res.sendStatus(200);
+})
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Listenting on PORT ${process.env.PORT}`);
